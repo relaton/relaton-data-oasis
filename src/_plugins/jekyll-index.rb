9d9b39d14f1291bd01e4fadf94d2a3c3882dfbd2
Andrei Kislichenko <android.2net@gmail.com>
2023-06-23 21:30:04 -0400

    update GH pages

--- src/_plugins/jekyll-index.rb
+++ src/_plugins/jekyll-index.rb
@@ -1,13 +1,41 @@
 module JekyllIndex
+  #
+  # JekyllIndex::Generator
+  #
   class Generator < Jekyll::Generator
     safe true
     priority :low
 
     def generate(site)
-      puts "Generating index..."
-      Jekyll.logger.info "Generating index..."
-      File.write "jekyll-index.log", "Generating index..."
-      raise "No index template found" unless site.layouts.key? 'index'
+      @site = site
+      collection.each { |item| site.posts.docs << create_doc(item) }
+      site.site_payload['site']['favicon'] = site.config['jekyll-index']['favicon']
+    end
+
+    private
+
+    def create_doc(item) # rubocop:disable Metrics/AbcSize
+      hash = YAML.load_file(item[:file])
+      doctype = hash['doctype']
+      date = hash['date'].find { |d| d['type'] == 'published' } || hash['date'].first
+      stage = hash.dig('docstatus', 'stage', 'value') || date['type']
+      doc = Jekyll::Document.new(item[:file], site: @site, collection: @site.collections['posts'])
+      doc.content = hash['title'].first['content']
+      yaml_ref = "#{@site.config['jekyll-index']['baseurl']}#{item[:file]}"
+      doc.merge_data!({ 'ref' => item[:id], 'doctype' => doctype, 'stage' => stage, 'date' => date['value'], "yaml_ref" => yaml_ref })
+      doc
+    end
+
+    def collection
+      return @collection if defined? @collection
+
+      @collection = index.map do |item|
+        item
+      end
+    end
+
+    def index
+      YAML.load_file(@site.config['jekyll-index']['source'])
     end
   end
 end
